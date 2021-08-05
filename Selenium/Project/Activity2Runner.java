d0820e29667ed16240fc8e8522b30e202dacea5a
LakshmiRajend <87632857+LakshmiRajend@users.noreply.github.com>
2021-08-05 11:02:57 +0530

    Add files via upload

---
+++ Selenium/Project/Activity2Runner.java
@@ -0,0 +1,17 @@
+package cucumberTests;
+
+import org.junit.runner.RunWith;
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+    features = "Features",
+    glue = {"stepDefinitions"},
+    tags = "@Activity2",
+    format = {"pretty", "html:target/cucumber"}
+)
+
+public class Activity2Runner {
+    //empty
+}
